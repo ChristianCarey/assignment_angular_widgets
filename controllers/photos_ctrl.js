52b176ae0a0b2093cf05013ccd875f8c913ab230
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-20 16:49:49 -0500

    Implement hash tag filtering

--- controllers/photos_ctrl.js
+++ controllers/photos_ctrl.js
@@ -8,17 +8,36 @@ Photos.controller('PhotosCtrl',
       $scope.instagramFilter;
       $scope.instagramFilters = ['Normal', 'Reyes', 'Lark',
                                  'Valencia', 'Inkwell', 'Ludwig'];
-      $scope.selectedHashtags = {
-        selected: []
+      $scope.allHashtags = [];
+      $scope.selectedHashtags = [];
+
+      $scope.toggleSelection = function(hashtag){
+        var index = $scope.selectedHashtags.indexOf(hashtag);
+        if (index > -1){
+          $scope.selectedHashtags.splice(index, 1);
+        } else {
+          $scope.selectedHashtags.push(hashtag);
+        }
       };
-      
-      $scope.allHashtags;
+
+      $scope.hashtagFilter = function(image){
+        var selectedLength = $scope.selectedHashtags.length;
+        var imageTagLength = image.tags.length;
+        for (var i = 0; i < imageTagLength; i++){
+          for (var j = 0; j < selectedLength; j++){
+            if (image.tags[i] === $scope.selectedHashtags[j]){
+              return true;
+            }
+          }
+        }
+        return false;
+      };
 
       (function(){
-        $scope.allHashtags = [];
         $scope.images.forEach(function(image){
           image.tags.forEach(function(tag){
             $scope.allHashtags.push(tag);
+            $scope.selectedHashtags.push(tag);
           });
         });
       }());
