23010b4068ee3bea13eeb7fe8e392a6bae3700dd
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-20 11:52:14 -0500

    Complete restaurant creation and deletion

--- controllers/restaurant_ctrl.js
+++ controllers/restaurant_ctrl.js
@@ -1,14 +1,29 @@
+
 Restaurant.controller('RestaurantCtrl', ['$scope', function($scope){
+
   $scope.restaurants = [];
+  $scope.image;
   $scope.name;
   $scope.typeOfFood;
+
   $scope.createRestaurant = function(valid) {
     if (!valid) { return };
+
     var restaurant = {};
+
+    restaurant.image = $scope.image;
     restaurant.name = $scope.name;
     restaurant.typeOfFood = $scope.typeOfFood;
+
     $scope.restaurants.push(restaurant);
+
+    $scope.image = undefined;
     $scope.name = undefined;
     $scope.typeOfFood = undefined;
   };
+
+  $scope.deleteRestaurant = function(index) {
+    $scope.restaurants.splice(index, 1);
+  };
+
 }]);
