c801f812fa938afb5edca236a4996ac167d620f8
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-20 14:00:30 -0500

    Implement hashtag menu

--- controllers/photos_ctrl.js
+++ controllers/photos_ctrl.js
@@ -5,6 +5,20 @@ Photos.controller('PhotosCtrl',
     function($scope, instagramResponse) {
 
       $scope.images = instagramResponse.data;
+      $scope.instagramFilter;
+      $scope.instagramFilters = ['Normal', 'Reyes', 'Lark',
+                                 'Valencia', 'Inkwell', 'Ludwig'];
+      $scope.selectedHashtags;
+      $scope.allHashtags;
 
-    }]);
+      (function(){
+        $scope.allHashtags = [];
+        $scope.images.forEach(function(image){
+          image.tags.forEach(function(tag){
+            $scope.allHashtags.push(tag);
+          });
+        });
+      }());
+
+}]);
 
